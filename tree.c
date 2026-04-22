f62267b75bad4486841459b06e72a7dc8a30e2cb
Deepti <deeptim2323@gmail.com>
2026-04-22 10:07:06 +0530

    phase2: loaded index

--- tree.c
+++ tree.c
@@ -132,6 +132,9 @@ int tree_serialize(const Tree *tree, void **data_out, size_t *len_out) {
 int tree_from_index(ObjectID *id_out) {
     // TODO: Implement recursive tree building
     // (See Lab Appendix for logical steps)
+    Index index;
+    if(index_load(&index)!=0) return -1;
+
     (void)id_out;
     return -1;
 }
